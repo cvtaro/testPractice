d049892965cd6ac9445a20d10b5f4b286ac870e2
mac taro <cvtaro.dev@gmail.com>
2021-09-11 07:13:25 +0900

    Coroutineを使ってProgressBar制御を実装

--- app/src/main/java/com/example/testpractice/MainActivity.kt
+++ app/src/main/java/com/example/testpractice/MainActivity.kt
@@ -2,10 +2,59 @@ package com.example.testpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Log.DEBUG
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.*
+import kotlin.time.measureTime
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var text : TextView
+    private lateinit var progressBar : ProgressBar
+    private lateinit var button01: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        text = findViewById(R.id.mainText)
+        text.apply {
+            text = ""
+        }
+        progressBar = findViewById(R.id.mainProgressBar)
+        progressBar.apply {
+            min = 0
+            max = 100
+            progress = 10
+        }
+
+        // クリックイベント
+        button01 = findViewById<Button>(R.id.mainButton01)
+        button01.setOnClickListener {
+            CoroutineScope(Dispatchers.Default).launch {
+                progressTest()
+            }
+        }
     }
-}
+
+
+    private suspend fun progressTest() {
+        withContext(Dispatchers.Main) {
+            button01.isEnabled = false
+        }
+        for (loop_i in 1..100) {
+            withContext(Dispatchers.Main) {
+                progressBar.progress = loop_i
+            }
+            Thread.sleep(25)
+        }
+        withContext(Dispatchers.Main) {
+            button01.isEnabled = true
+        }
+    }
+} // end class
